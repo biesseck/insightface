b67776a2560d9018feacad471b48f8f86a6e9569
Bernardo Biesseck <biesseck@gmail.com>
2026-01-26 23:50:15 -0300

    Add config files 'recognition/arcface_torch/configs/casiawebface_merge_Synth_137subj_Arc2Face_similarity=[50,59]_r50.py' and 'recognition/arcface_torch/configs/casiawebface_merge_Synth_106subj_Arc2Face_similarity=[60,69]_r50.py'

---
+++ recognition/arcface_torch/configs/casiawebface_merge_Synth_106subj_Arc2Face_similarity=[60,69]_r50.py
@@ -0,0 +1,108 @@
+from easydict import EasyDict as edict
+import os
+uname = os.uname()
+
+# make training faster
+# our RAM is 256G
+# mount -t tmpfs -o size=140G  tmpfs /train_tmp
+
+config = edict()
+config.margin_list = (1.0, 0.5, 0.0)
+config.network = "r50"
+config.resume = False
+config.output = None
+config.embedding_size = 512
+config.sample_rate = 1.0
+config.fp16 = True
+config.momentum = 0.9
+config.weight_decay = 5e-4
+# config.batch_size = 128
+config.batch_size = 256
+config.lr = 0.1
+config.verbose = 2000
+# config.verbose = 10
+config.dali = False
+
+
+
+
+if uname.nodename == 'duo':
+    # config.rec = "/train_tmp/faces_emore"
+    # config.rec = '/datasets2/frcsyn_wacv2024/datasets/real/1_CASIA-WebFace/faces_webface_112x112'     # duo
+    config.rec = '/datasets2/frcsyn_wacv2024/datasets/real/1_CASIA-WebFace/imgs_crops_112x112'          # duo
+
+    # config.val_targets = ['lfw', 'cfp_fp', "agedb_30"]
+    # config.val_targets = ['']
+    # config.val_targets = ['bupt']
+    config.val_targets = ['/datasets2/frcsyn_wacv2024/datasets/real/1_CASIA-WebFace/faces_webface_112x112/lfw.bin', '/datasets2/frcsyn_wacv2024/datasets/real/1_CASIA-WebFace/faces_webface_112x112/cfp_fp.bin', '/datasets2/frcsyn_wacv2024/datasets/real/1_CASIA-WebFace/faces_webface_112x112/agedb_30.bin', 'bupt']
+    config.val_dataset_dir = ['/datasets2/frcsyn_wacv2024/datasets/real/3_BUPT-BalancedFace/race_per_7000_crops_112x112']
+    config.val_protocol_path = ['/datasets2/frcsyn_wacv2024/comparison_files/comparison_files/sub-tasks_1.1_1.2/bupt_comparison.txt']
+
+elif uname.nodename == 'diolkos':
+    config.rec = '/nobackup/unico/datasets/face_recognition/1_CASIA-WebFace/imgs_crops_112x112'   # diolkos
+
+    # config.val_targets = ['/nobackup/unico/datasets/face_recognition/1_CASIA-WebFace/faces_webface_112x112/lfw.bin']
+    config.val_targets = ['/nobackup/unico/datasets/face_recognition/1_CASIA-WebFace/faces_webface_112x112/lfw.bin', '/nobackup/unico/datasets/face_recognition/1_CASIA-WebFace/faces_webface_112x112/cfp_fp.bin', '/nobackup/unico/datasets/face_recognition/1_CASIA-WebFace/faces_webface_112x112/agedb_30.bin']
+    # config.val_targets = ['/nobackup/unico/datasets/face_recognition/1_CASIA-WebFace/faces_webface_112x112/lfw.bin', '/nobackup/unico/datasets/face_recognition/1_CASIA-WebFace/faces_webface_112x112/cfp_fp.bin', '/nobackup/unico/datasets/face_recognition/1_CASIA-WebFace/faces_webface_112x112/agedb_30.bin', 'bupt']
+    # config.val_targets = ['bupt']
+    
+    # config.val_dataset_dir = ['/nobackup/unico/frcsyn_wacv2024/datasets/real/3_BUPT-BalancedFace/race_per_7000_crops_112x112']
+    # config.val_protocol_path = ['/nobackup/unico/frcsyn_wacv2024/comparison_files/comparison_files/sub-tasks_1.1_1.2/bupt_comparison.txt']
+
+
+    config.path_subjs_list_to_merge = '/nobackup/unico/datasets/face_recognition/1_CASIA-WebFace/merge_with_dataset_MS-Celeb-1M-ms1m-retinaface-t1-imgs_FACE_EMBEDDINGS_sim-range=[0.5,0.69]/dict_paths_new_subjs_base_subjs.json'
+
+elif uname.nodename == 'cedro':
+    config.rec = '/hddevice/nobackup3/bjgbiesseck/datasets/face_recognition/CASIA-WebFace/imgs_crops_112x112'
+
+    config.val_targets = ['/hddevice/nobackup3/bjgbiesseck/datasets/face_recognition/CASIA-WebFace/faces_webface_112x112/lfw.bin', '/hddevice/nobackup3/bjgbiesseck/datasets/face_recognition/CASIA-WebFace/faces_webface_112x112/cfp_fp.bin', '/hddevice/nobackup3/bjgbiesseck/datasets/face_recognition/CASIA-WebFace/faces_webface_112x112/agedb_30.bin']
+    # config.val_targets = ['/nobackup/unico/datasets/face_recognition/1_CASIA-WebFace/faces_webface_112x112/lfw.bin', '/nobackup/unico/datasets/face_recognition/1_CASIA-WebFace/faces_webface_112x112/cfp_fp.bin', '/nobackup/unico/datasets/face_recognition/1_CASIA-WebFace/faces_webface_112x112/agedb_30.bin', 'bupt']
+    # config.val_targets = ['bupt']
+    
+    # config.val_dataset_dir = ['/nobackup/unico/frcsyn_wacv2024/datasets/real/3_BUPT-BalancedFace/race_per_7000_crops_112x112']
+    # config.val_protocol_path = ['/nobackup/unico/frcsyn_wacv2024/comparison_files/comparison_files/sub-tasks_1.1_1.2/bupt_comparison.txt']
+
+
+    config.path_other_dataset = '/hddevice/nobackup3/bjgbiesseck/datasets/face_recognition/CASIA-WebFace/imgs_crops_112x112_FACE_EMBEDDINGS_newSynthIDs_Arc2Face_sim=[0.6,0.69]_106ids_DETECTED_FACES_RETINAFACE_scales=[1.0]_nms=0.4/imgs'
+
+
+else:
+    raise Exception(f'Paths of train and val datasets could not be found in file \'{__file__}\'')
+
+
+
+# config.num_classes = 85742
+config.num_classes = 10572
+
+# config.num_image = 5822653
+config.num_image = 490623
+
+
+config.num_epoch = 20
+# config.num_epoch = 30
+config.warmup_epoch = 0
+
+
+
+# WandB Logger
+# config.wandb_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+config.wandb_key = "d7664714c72bd594a957381812da450279f80f66"
+
+config.suffix_run_name = None
+
+config.using_wandb = False
+# config.using_wandb = True
+
+# config.wandb_entity = "entity"
+# config.wandb_entity = "bovifocr"
+config.wandb_entity = "bjgbiesseck"
+
+config.wandb_project = "R50_CASIA-Webface_10572classes_whole_dataset"
+config.wandb_log_all = True
+
+# config.save_artifacts = False
+config.save_artifacts = True
+
+config.wandb_resume = False # resume wandb run: Only if the you wand t resume the last run that it was interrupted
+
+config.notes = ''
